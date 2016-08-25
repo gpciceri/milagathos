edd93a0558fadb2cb1c717a310d343ac50c5474d
gpciceri <gp.ciceri@gmail.com>
2016-08-25 08:44:28 +0200

    Create sixWeeksExercises.py

---
+++ SixWeeksPreparationForReadingCaesar/pgm/sixWeeksExercises.py
@@ -0,0 +1,101 @@
+#
+# -*- coding: utf-8 -*-
+'''/* sixWeeksExercises.py
+ *
+ * Copyright (C) 2016 Gian Paolo Ciceri
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * Author:
+ * 	gian paolo ciceri <gp.ciceri@gmail.com>
+ *
+ *
+ * Release:
+ *  2016.08.24 - initial release.
+ *
+ */
+'''
+# ā ă ē ĕ ī ĭ ō ū ŭ ŏ
+#
+# ('', '', '')
+
+
+SIXWEEKS_PAG11_VOC_EX1 = [
+# VOCABOLARIO DAL LATINO
+# LAT, ITA, ENG
+('silva, silvae, f.', 'la foresta', 'forest'),
+('fossa, fossae, f.', 'il fossato', 'ditch'),
+('causa, causae, f.', 'la causa', 'cause'),
+('pugna, pugnae, f.', 'il combattimento', 'fight'),
+('porta, portae, f.', 'la porta', 'gate'),
+('summa, summae, f.', "l'insieme", 'sum'),
+('puella, puellae, f.', 'la ragazza', 'girl'),
+('ripa, ripae, f.', 'la riva', 'bank'),
+('cura, curae, f.', 'la cura', 'care'),
+('fuga, fugae, f.', 'la fuga', 'flight'),
+('via, viae, f.', 'la via', 'way'),
+('mora, morae, f.', 'il ritardo', 'delay'),
+('poena, poenae, f.', 'la pena', 'punishment'),
+('copiae, copiarum, f.pl.', 'le truppe', 'forces'),
+]
+
+SIXWEEKS_PAG11_EX1 = [
+#EX1 (DAL LATINO)
+# LAT, ITA, ENG
+('portarum', 'delle porte', 'of the gates'),
+('pugnā', 'con il combattimento', 'by a fight'),
+('causae', 'della causa, alla causa, le cause, o cause', 'of the cause, to the cause, the causes, causes'), 
+('fossas', '(pulisci) i fossati', '(clean) the ditch'),
+('silvam', '(abbatti) la foresta', '(cut) the forest'),
+('portas', '(apri) le porte', '(open) the gates'),
+('silvis', 'alle foreste, con le foreste', 'to the forests, with the forests'),
+('causarum', 'delle cause', 'of the causes'),
+('ripae', 'della riva, alla riva, le rive, o rive', 'of the bank, to the bank, the banks,banks'), 
+('moras', '(fare) ritardi', '(perform) lates'),
+('summas', '(fare) le somme', '(do) the sums'),
+('morā', 'con il ritardo', 'by a late'),
+('ripis', 'alle rive, con le rive', 'to the banks, with the banks'),
+('copiarum summa', "l'insieme delle truppe", 'the set of the forces'),
+('pugnae morā', ' a causa del ritardo del combattimento', 'the cause the delay of the fight'),
+('fossarum ripas', '(pulisci) le rive dei fossati', '(clean) the banks of the ditches'),
+('viae curam', '(trova) la cura della via', '(find) the care of the way'),
+('pugnarum causā', 'per causa dei combattimenti', 'for cause of the fights'),
+('portae curā', 'per cura della porta', 'for the care of the door,'),
+('copiarum fugā', 'a causa della fuga delle truppe', 'a cause of the flight of the forces'),
+('puellae causā', 'a causa della ragazza', 'for the cause of the girl'), 
+('puellarum poenam', '(immaginate) la pena delle ragazze', '(image) the punishment of the girls'),
+]
+
+SIXWEEKS_PAG11_EX2 = [
+#EX2 (DALL'INGLESE)
+# LAT, ITA, ENG
+('portae', 'della porta', 'of a gate'),
+('causa copiarum', 'per causa delle truppe', 'for the forces'),
+('copiis', 'per mezzo delle truppe', 'for the forces'),
+('morā', 'con il ritardo', 'by delay'),
+('pugnā', 'con il combattimento)', 'by fight'),
+('curarum', 'delle cure', 'of cares'),
+('causae', 'le cause, della causa, alla causa', 'causes, of a cause, to a cause'),
+('vias', '(guardo) le vie', '(look) ways'),
+('fossarum ripa', 'presso la riva dei fossati', 'by the bank of the ditches'),
+('copiarum fugae', 'alla fuga delle truppe', 'to the flight of the forces'),
+('portarum cura', 'la cura delle porte', 'the care of the gate'),
+('curas viarum', '(guardo) le cure delle vie', 'the cares of the ways'),
+('causa portarum', 'per conto delle porte', 'on account of the gates'),
+('morarum causā ', 'per causa dei ritardi', 'for the sake of delay'),
+('poenarum causā', 'per causa delle pene', 'for the sake of punishment'),
+('puellarum fugā', 'per la fuga delle ragazze', 'by the flight of the girls'),
+
+]
